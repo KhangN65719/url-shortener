86f93f1fa69514a5e1942861835fa09b0bc07e29
Khang Nguyen <Winnguyen0919@gmail.com>
2026-06-09 02:48:00 -0500

    Packaged Modules for the CLI

---
+++ internal/client/client.go
@@ -0,0 +1,47 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Link struct{
+	Url string `json:"url"`
+}
+
+func SendURL(url string){
+	jsonInstance := Link{Url : url}
+	jsonBody, err := json.Marshal(jsonInstance)
+
+	if err != nil{
+		return
+	}
+
+	req, err := http.NewRequest("POST", "http://localhost:6767/shorten", bytes.NewReader(jsonBody))
+	
+	if err != nil{
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil{
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil{
+		return
+	}
+
+	fmt.Print(string(body))
+}
---
+++ internal/validate/validate.go
@@ -0,0 +1,21 @@
+package validate
+
+import(
+	"net/url"
+	"fmt"
+)
+
+func ValidateLongURL(raw string) (bool, error) {
+    u, err := url.ParseRequestURI(raw)
+    if err != nil {
+        return false, fmt.Errorf("invalid URL: %w", err)
+    }
+    if u.Scheme != "http" && u.Scheme != "https" {
+        return false, fmt.Errorf("URL must start with http:// or https://")
+    }
+    if u.Host == "" {
+        return false, fmt.Errorf("URL must have a valid host")
+    }
+    return true, nil
+}
+
